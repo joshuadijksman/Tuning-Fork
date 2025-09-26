17c67390b1f4269e85e592d4c29d13966e89c660
PierFelix <pietertoor@gmail.com>
2025-09-26 15:36:40 +0200

    less white space around the plot

--- frequencySweep2D.py
+++ frequencySweep2D.py
@@ -6,12 +6,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def heatmapPlot(fName: str, data: list[list[int|float]] | np.ndarray, xTickLabels: list[int|float] = [], yTickLabels: list[int|float] = [], title: str = "Amplitude", xTicks: int = 10, yTicks: int = 10, xLabelRound: int = 4, yLabelRound: int = 4) -> None:
-    fig = plt.figure()
+def heatmapPlot(fName: str, data: list[list[int|float]] | np.ndarray, xTickLabels: list[int|float] = [], yTickLabels: list[int|float] = [], title: str = "", xTicks: int = 10, yTicks: int = 10, xTickLabelRound: int = 4, yTickLabelRound: int = 4, figsize=(9,6)) -> None:
+    fig = plt.figure(None, figsize)
     
     ax = sns.heatmap(data)
 
     if len(xTickLabels)!=0:
+        ax.set_xlabel("Shear Frequency (Hz)")
         if len(xTickLabels)>=xTicks: 
             xStep = int(len(data[0])/xTicks)
             xSpots = list(range(0, len(data[0])+1, xStep))
@@ -22,10 +23,11 @@ def heatmapPlot(fName: str, data: list[list[int|float]] | np.ndarray, xTickLabel
             else:
                 step = int(len(xTickLabels)/xTicks)
                 for i, _ in enumerate(xSpots):
-                    xSpots[i] = round(xTickLabels[i*step], xLabelRound)
+                    xSpots[i] = round(xTickLabels[i*step], xTickLabelRound)
                 ax.set_xticklabels(xSpots)
         
     if len(yTickLabels)!=0:
+        ax.set_ylabel("Normal Frequency (Hz)")
         if len(yTickLabels)>=yTicks:
             yStep = int(len(data)/yTicks)
             ySpots = list(range(0, len(data)+1, yStep))
@@ -36,16 +38,14 @@ def heatmapPlot(fName: str, data: list[list[int|float]] | np.ndarray, xTickLabel
             else:
                 step = int(len(yTickLabels)/yTicks)
                 for i, _ in enumerate(ySpots):
-                    ySpots[i] = round(yTickLabels[i*step], yLabelRound)
+                    ySpots[i] = round(yTickLabels[i*step], yTickLabelRound)
                 ax.set_yticklabels(ySpots)
-
-    ax.set_ylabel("Normal Frequency (Hz)")
-    ax.set_xlabel("Shear Frequency (Hz)")
-
-    ax.set_title(title)
+    
+    if title != "":
+        ax.set_title(title)
 
     fig.axes.append(ax)
-    fig.savefig(fName)
+    fig.savefig(fName, bbox_inches="tight")
 
 
 def fileAvailable(folder, name, ext = ".csv", n=1) -> str:
