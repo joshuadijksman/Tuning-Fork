5583b8974782aadaeb719c2f423425e35add8127
PierFelix <pietertoor@gmail.com>
2025-10-09 15:44:43 +0200

    updated main for new experiment

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from newsfa import sfa
 import PLL
 from pi_e_625 import pi_e_625
 from mitutoyo import mitutoyo
+from measurements import frequencyDependence
+from rigol_dg1022 import RigolDG
 
 
 def calibrateAir(  
@@ -195,15 +197,52 @@ def calibrateDistance(ctrlNormal: sfa,
 def main() -> None:
     ctrlNormal: sfa = sfa(SN="A9JSTXTQA")
     ctrlShear: sfa = sfa(SN="A9TQAG5OA")
+    freqGen: RigolDG = RigolDG()
     zStage = pi_e_625()
     hDev = mitutoyo()
 
-    ctrlNormal.setAmplitude(0.006)
-    ctrlShear.setAmplitude(0.012)
+    freqGen.set_waveform(1, "SIN")
+    freqGen.set_waveform(2, "SIN")
+    freqGen.set_output(1, True)
+    freqGen.set_output(2, True)
 
-    optNormal, optShear = calibrateAir(ctrlNormal, ctrlShear)
+    # optNormal, optShear = calibrateAir(ctrlNormal, ctrlShear)
+    # calibrateDistance(ctrlNormal, zStage, hDev)
 
-    calibrateDistance(ctrlNormal, zStage, hDev)
+    findNorm = True
+
+    fNormalMin = 790
+    fNormalMax = 810
+
+    fShearMin = 400
+    fShearMax = 500
+
+
+    resolution = 0.01
+
+    if findNorm:
+        freqs = np.linspace(fShearMin, fShearMax, int((fShearMax-fShearMin)/resolution+1), endpoint=True)
+        frequencyDependence(
+            ctrlNormal,
+            ctrlShear,
+            freqGen,
+            790,
+            810,
+            freqs,
+            
+        )
+    else:
+        freqs = np.linspace(fNormalMin, fNormalMax, int((fNormalMax-fNormalMin)/resolution+1), endpoint=True)
+        frequencyDependence(
+            ctrlNormal,
+            ctrlShear,
+            freqGen,
+            fShearMin,
+            fShearMax,
+            freqs,
+            findNorm=False
+        )
+        
 
 
 
