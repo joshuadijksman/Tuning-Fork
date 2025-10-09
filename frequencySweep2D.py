6e697189c60b32ee79ec50b79b8d65803f4c1a6d
PierFelix <pietertoor@gmail.com>
2025-10-09 12:01:52 +0200

    moved heatmap plot to seperate file for all plots

--- frequencySweep2D.py
+++ frequencySweep2D.py
@@ -3,50 +3,8 @@ import os
 import time
 from datetime import timedelta
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 from rigol_dg1022 import RigolDG
-
-def heatmapPlot(fName: str, data: list[list[int|float]] | np.ndarray, xTickLabels: list[int|float] = [], yTickLabels: list[int|float] = [], title: str = "", xTicks: int = 10, yTicks: int = 10, xTickLabelRound: int = 4, yTickLabelRound: int = 4, figsize=(9,6)) -> None:
-    fig = plt.figure(None, figsize)
-    
-    ax = sns.heatmap(data)
-
-    if len(xTickLabels)!=0:
-        ax.set_xlabel("Shear Frequency (Hz)")
-        if len(xTickLabels)>=xTicks: 
-            xStep = int(len(data[0])/xTicks)
-            xSpots = list(range(0, len(data[0])+1, xStep))
-            ax.set_xticks(ticks=np.array(xSpots)+0.5)
-        
-            if len(xTickLabels)==xTicks:
-                ax.set_xticklabels(xTickLabels) 
-            else:
-                step = int(len(xTickLabels)/xTicks)
-                for i, _ in enumerate(xSpots):
-                    xSpots[i] = round(xTickLabels[i*step], xTickLabelRound)
-                ax.set_xticklabels(xSpots)
-        
-    if len(yTickLabels)!=0:
-        ax.set_ylabel("Normal Frequency (Hz)")
-        if len(yTickLabels)>=yTicks:
-            yStep = int(len(data)/yTicks)
-            ySpots = list(range(0, len(data)+1, yStep))
-            ax.set_yticks(np.array(ySpots)+0.5)
-
-            if len(yTickLabels)==yTicks:
-                ax.set_yticklabels(yTickLabels)
-            else:
-                step = int(len(yTickLabels)/yTicks)
-                for i, _ in enumerate(ySpots):
-                    ySpots[i] = round(yTickLabels[i*step], yTickLabelRound)
-                ax.set_yticklabels(ySpots)
-    
-    if title != "":
-        ax.set_title(title)
-
-    fig.axes.append(ax)
-    fig.savefig(fName, bbox_inches="tight")
+from plots import heatmapPlot
 
 
 def fileAvailable(folder, name, n=0) -> str:
@@ -159,6 +117,7 @@ def frequencySweep2D(
 
     ctrlNormal.ser.close()
     ctrlShear.ser.close()
+
 
 if __name__ == "__main__":
     resolution = 1
---
+++ plots.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def heatmapPlot(fName: str, data: list[list[int|float]] | np.ndarray, xTickLabels: list[int|float] = [], yTickLabels: list[int|float] = [], title: str = "", xTicks: int = 10, yTicks: int = 10, xTickLabelRound: int = 4, yTickLabelRound: int = 4, figsize=(9,6)) -> None:
+    fig = plt.figure(None, figsize)
+    
+    ax = sns.heatmap(data)
+
+    if len(xTickLabels)!=0:
+        ax.set_xlabel("Shear Frequency (Hz)")
+        if len(xTickLabels)>=xTicks: 
+            xStep = int(len(data[0])/xTicks)
+            xSpots = list(range(0, len(data[0])+1, xStep))
+            ax.set_xticks(ticks=np.array(xSpots)+0.5)
+        
+            if len(xTickLabels)==xTicks:
+                ax.set_xticklabels(xTickLabels) 
+            else:
+                step = int(len(xTickLabels)/xTicks)
+                for i, _ in enumerate(xSpots):
+                    xSpots[i] = round(xTickLabels[i*step], xTickLabelRound)
+                ax.set_xticklabels(xSpots)
+        
+    if len(yTickLabels)!=0:
+        ax.set_ylabel("Normal Frequency (Hz)")
+        if len(yTickLabels)>=yTicks:
+            yStep = int(len(data)/yTicks)
+            ySpots = list(range(0, len(data)+1, yStep))
+            ax.set_yticks(np.array(ySpots)+0.5)
+
+            if len(yTickLabels)==yTicks:
+                ax.set_yticklabels(yTickLabels)
+            else:
+                step = int(len(yTickLabels)/yTicks)
+                for i, _ in enumerate(ySpots):
+                    ySpots[i] = round(yTickLabels[i*step], yTickLabelRound)
+                ax.set_yticklabels(ySpots)
+    
+    if title != "":
+        ax.set_title(title)
+
+    fig.axes.append(ax)
+    fig.savefig(fName, bbox_inches="tight")
