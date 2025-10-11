848b8014ee6e550544b203414ac6e4f97e20dd03
PierFelix <pietertoor@gmail.com>
2025-10-11 14:50:13 +0200

    used function from newsfa instead of writing the function twice

--- mitutoyo.py
+++ mitutoyo.py
@@ -1,16 +1,13 @@
 import serial
 from serial.tools import list_ports
 from numpy import nan
+from newsfa import find_unique_dev_by_pidvid
 
-def port_find_unique_dev_by_pidvid(pid: int, vid: int):
-    found_devices = list(filter(lambda p: p.pid == pid and p.vid == vid, list_ports.comports()))
-    print("Found Mitutoyo device(s):", found_devices)
-    return found_devices[0] if len(found_devices) == 1 else None
 
 class mitutoyo(object):
-    def __init__(self, port='') -> None:
+    def __init__(self, port="") -> None:
         if port == "":
-            port = str(port_find_unique_dev_by_pidvid(0x4001, 0x0fe7)).split(" ")[0]
+            port = str(find_unique_dev_by_pidvid(0x4001, 0x0FE7)).split(" ")[0]
             print("Using Mitutoyo port:", port)
         self.ser = serial.Serial(port=port, baudrate=115200)
 
@@ -19,13 +16,13 @@ class mitutoyo(object):
         a: str = ""
         c: str = ""
 
-        while c != '\r':
+        while c != "\r":
             c = self.ser.read().decode()
 
-            if c != '\r':
+            if c != "\r":
                 a += c
 
-        if f == '9':
+        if f == "9":
             print("Error")
 
         return a
@@ -33,17 +30,17 @@ class mitutoyo(object):
     def measurement(self) -> float:
         """
         Measures height
-        
+
         Returns height value or nan on bad read.
         """
-        m: float = 0.
+        m: float = 0.0
         cmd = "1\r".encode()
         self.ser.write(cmd)
-        a = self.answer().split('\r')[0]
+        a = self.answer().split("\r")[0]
 
-        if a.startswith('1A'):
+        if a.startswith("1A"):
             try:
-                m = float(a.replace('1A', ""))
+                m = float(a.replace("1A", ""))
             except ValueError:
                 m = nan
         return m
