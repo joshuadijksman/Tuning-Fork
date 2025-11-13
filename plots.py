6b915e13159e4451aaaa6deaf5381b5d9747fa34
PierFelix <pietertoor@gmail.com>
2025-11-13 14:19:24 +0100

    updated heatplotmap to include the changes that lineplot had. (and remove the errors)

--- plots.py
+++ plots.py
@@ -13,49 +13,70 @@ def heatmapPlot(
     xTickLabels: list[int | float] = [],
     yTickLabels: list[int | float] = [],
     title: str = "",
+    xLabel: str = "",
+    yLabel: str = "",
     xTicks: int = 10,
     yTicks: int = 10,
-    xTickLabelRound: int = 4,
-    yTickLabelRound: int = 4,
     figsize: tuple[int, int] = (9, 6),
+    **kwargs
 ) -> None:
     fig = plt.figure(None, figsize)
 
     ax = sns.heatmap(data)
 
+    xDecimals: int = kwargs.get('xDecimals', 3)
+    xticks = np.linspace(data[0][0], data[0][-1], xTicks, endpoint=True)
     if len(xTickLabels) != 0:
         if len(xTickLabels) >= xTicks:
-            xStep = int(len(data[0]) / xTicks)
-            xSpots = list(range(0, len(data[0]) + 1, xStep))
-            ax.set_xticks(ticks=np.array(xSpots) + 0.5)
+            ax.set_xticks(ticks=xticks)
 
             if len(xTickLabels) == xTicks:
-                ax.set_xticklabels(xTickLabels)
+                ax.set_xticklabels([str(label) for label in xTickLabels])
             else:
                 step = int(len(xTickLabels) / xTicks)
-                for i, _ in enumerate(xSpots):
-                    xSpots[i] = round(xTickLabels[i * step], xTickLabelRound)
-                ax.set_xticklabels(xSpots)
-
+                for i, _ in enumerate(xticks):
+                    xticks[i] = xTickLabels[i * step]
+                ax.set_xticklabels(xticks)
+                ax.xaxis.set_major_formatter(FormatStrFormatter(f'%.{xDecimals}f'))
+    elif xTicks != 0:
+        ax.set_xticks(ticks=xticks, labels=xticks)
+        ax.xaxis.set_major_formatter(formatter=FormatStrFormatter(f'%.{xDecimals}f'))
+        ax.tick_params(axis='x', rotation=30)
+    else:
+        ax.tick_params(axis='x', which='both', bottom=False, labelbottom=False)
+    
+    yDecimals: int = kwargs.get('yDecimals', 3)    
+    yticks = np.round(np.linspace(min(data[1]), max(data[1]), yTicks, endpoint=True), decimals=yDecimals)
     if len(yTickLabels) != 0:
         if len(yTickLabels) >= yTicks:
-            yStep = int(len(data) / yTicks)
-            ySpots = list(range(0, len(data) + 1, yStep))
-            ax.set_yticks(np.array(ySpots) + 0.5)
+            ax.set_yticks(yticks)
 
             if len(yTickLabels) == yTicks:
-                ax.set_yticklabels(yTickLabels)
+                ax.set_yticklabels([str(label) for label in yTickLabels])
             else:
                 step = int(len(yTickLabels) / yTicks)
-                for i, _ in enumerate(ySpots):
-                    ySpots[i] = round(yTickLabels[i * step], yTickLabelRound)
-                ax.set_yticklabels(ySpots)
+                for i, _ in enumerate(yticks):
+                    yticks[i] = round(yTickLabels[i * step], yDecimals)
+                ax.set_yticklabels(yticks)
+                ax.yaxis.set_major_formatter(FormatStrFormatter(f'%.{yDecimals}f'))
+    elif yTicks != 0:
+        ax.set_yticks(ticks=yticks, labels=yticks)
+        ax.yaxis.set_major_formatter(FormatStrFormatter(f'%.{yDecimals}f'))
+    else:
+        ax.tick_params(axis='y', which='both', left=False, labelleft=False, right=False, labelright=False)
 
     if title != "":
         ax.set_title(title)
+        
+    if xLabel != "":
+        ax.set_xlabel(xLabel)
+    else:
+        ax.set_xlabel("Shear Frequency (Hz)")
 
-    ax.set_xlabel("Shear Frequency (Hz)")
-    ax.set_ylabel("Normal Frequency (Hz)")
+    if yLabel != "":
+        ax.set_ylabel(yLabel)
+    else:
+        ax.set_ylabel("Normal Frequency (Hz)")
 
     fig.axes.append(ax)
     fig.savefig(fName, bbox_inches="tight")
