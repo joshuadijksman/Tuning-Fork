d616b135e13293ad01cb6e36d0c62d9231659be9
PierFelix <pietertoor@gmail.com>
2025-11-27 10:57:09 +0100

    added fmt argument to lineplot and some auto formatting

--- plots.py
+++ plots.py
@@ -18,13 +18,13 @@ def heatmapPlot(
     xTicks: int = 10,
     yTicks: int = 10,
     figsize: tuple[int, int] = (9, 6),
-    **kwargs
+    **kwargs,
 ) -> None:
     fig = plt.figure(None, figsize)
 
     ax = sns.heatmap(data)
 
-    xDecimals: int = kwargs.get('xDecimals', 3)
+    xDecimals: int = kwargs.get("xDecimals", 3)
     xticks = np.linspace(data[0][0], data[0][-1], xTicks, endpoint=True)
     if len(xTickLabels) != 0:
         if len(xTickLabels) >= xTicks:
@@ -37,16 +37,19 @@ def heatmapPlot(
                 for i, _ in enumerate(xticks):
                     xticks[i] = xTickLabels[i * step]
                 ax.set_xticklabels(xticks)
-                ax.xaxis.set_major_formatter(FormatStrFormatter(f'%.{xDecimals}f'))
+                ax.xaxis.set_major_formatter(FormatStrFormatter(f"%.{xDecimals}f"))
     elif xTicks != 0:
         ax.set_xticks(ticks=xticks, labels=xticks)
-        ax.xaxis.set_major_formatter(formatter=FormatStrFormatter(f'%.{xDecimals}f'))
-        ax.tick_params(axis='x', rotation=30)
+        ax.xaxis.set_major_formatter(formatter=FormatStrFormatter(f"%.{xDecimals}f"))
+        ax.tick_params(axis="x", rotation=30)
     else:
-        ax.tick_params(axis='x', which='both', bottom=False, labelbottom=False)
-    
-    yDecimals: int = kwargs.get('yDecimals', 3)    
-    yticks = np.round(np.linspace(min(data[1]), max(data[1]), yTicks, endpoint=True), decimals=yDecimals)
+        ax.tick_params(axis="x", which="both", bottom=False, labelbottom=False)
+
+    yDecimals: int = kwargs.get("yDecimals", 3)
+    yticks = np.round(
+        np.linspace(min(data[1]), max(data[1]), yTicks, endpoint=True),
+        decimals=yDecimals,
+    )
     if len(yTickLabels) != 0:
         if len(yTickLabels) >= yTicks:
             ax.set_yticks(yticks)
@@ -58,16 +61,23 @@ def heatmapPlot(
                 for i, _ in enumerate(yticks):
                     yticks[i] = round(yTickLabels[i * step], yDecimals)
                 ax.set_yticklabels(yticks)
-                ax.yaxis.set_major_formatter(FormatStrFormatter(f'%.{yDecimals}f'))
+                ax.yaxis.set_major_formatter(FormatStrFormatter(f"%.{yDecimals}f"))
     elif yTicks != 0:
         ax.set_yticks(ticks=yticks, labels=yticks)
-        ax.yaxis.set_major_formatter(FormatStrFormatter(f'%.{yDecimals}f'))
+        ax.yaxis.set_major_formatter(FormatStrFormatter(f"%.{yDecimals}f"))
     else:
-        ax.tick_params(axis='y', which='both', left=False, labelleft=False, right=False, labelright=False)
+        ax.tick_params(
+            axis="y",
+            which="both",
+            left=False,
+            labelleft=False,
+            right=False,
+            labelright=False,
+        )
 
     if title != "":
         ax.set_title(title)
-        
+
     if xLabel != "":
         ax.set_xlabel(xLabel)
     else:
@@ -86,7 +96,7 @@ def heatmapPlot(
 def linePlot(
     fName: str | PathLike,
     x: list[int | float] | np.ndarray,
-    y: list[int | float] | np.ndarray
+    y: list[int | float] | np.ndarray,
 ) -> None: ...
 @overload
 def linePlot(
@@ -113,7 +123,7 @@ def linePlot(
     figsize: tuple[int, int] = (9, 6),
     linestyle: str = "-",
     transparent: bool = False,
-    dpi: float | None = None
+    dpi: float | None = None,
 ) -> None: ...
 @overload
 def linePlot(
@@ -132,7 +142,7 @@ def linePlot(
     figsize: tuple[int, int] = (9, 6),
     linestyle: str = "-",
     transparent: bool = False,
-    dpi: float | None = None
+    dpi: float | None = None,
 ) -> None: ...
 
 
@@ -150,7 +160,7 @@ def linePlot(
     xTicks: int = 10,
     yTicks: int = 10,
     figsize: tuple[int, int] = (9, 6),
-    **kwargs
+    **kwargs,
 ) -> None:
     """
     Creates a line plot figure with basic settings
@@ -165,7 +175,7 @@ def linePlot(
 
     :param y: y axis data points
     :type y: list[int | float] | ndarray
-    
+
     :param time: time data for x axis
     :type time: list[int | float] | ndarray
 
@@ -183,13 +193,16 @@ def linePlot(
 
     :param yDecimal: Amount of decimal points on the Y-axis
     :type yDecimal: int
+
+    :param fmt: plot line format, string of options \"[marker][line][colour]\"
+    :type fmt: str
     """
     fig = plt.figure(None, figsize, dpi=kwargs.get("dpi", None))
     ax = fig.add_subplot()
-    ax.plot(x, y, linestyle=kwargs.get("linestyle", "-"))
+    ax.plot(x, y, kwargs.get("fmt", ""))
     ax.grid(visible=True)
-    
-    xDecimals: int = kwargs.get('xDecimals', 3)
+
+    xDecimals: int = kwargs.get("xDecimals", 3)
     xticks = np.linspace(x[0], x[-1], xTicks, endpoint=True)
     if len(xTickLabels) != 0:
         if len(xTickLabels) >= xTicks:
@@ -202,16 +215,18 @@ def linePlot(
                 for i, _ in enumerate(xticks):
                     xticks[i] = xTickLabels[i * step]
                 ax.set_xticklabels(xticks)
-                ax.xaxis.set_major_formatter(FormatStrFormatter(f'%.{xDecimals}f'))
+                ax.xaxis.set_major_formatter(FormatStrFormatter(f"%.{xDecimals}f"))
     elif xTicks != 0:
         ax.set_xticks(ticks=xticks, labels=xticks)
-        ax.xaxis.set_major_formatter(formatter=FormatStrFormatter(f'%.{xDecimals}f'))
-        ax.tick_params(axis='x', rotation=30)
+        ax.xaxis.set_major_formatter(formatter=FormatStrFormatter(f"%.{xDecimals}f"))
+        ax.tick_params(axis="x", rotation=30)
     else:
-        ax.tick_params(axis='x', which='both', bottom=False, labelbottom=False)
-    
-    yDecimals: int = kwargs.get('yDecimals', 3)    
-    yticks = np.round(np.linspace(min(y), max(y), yTicks, endpoint=True), decimals=yDecimals)
+        ax.tick_params(axis="x", which="both", bottom=False, labelbottom=False)
+
+    yDecimals: int = kwargs.get("yDecimals", 3)
+    yticks = np.round(
+        np.linspace(min(y), max(y), yTicks, endpoint=True), decimals=yDecimals
+    )
     if len(yTickLabels) != 0:
         if len(yTickLabels) >= yTicks:
             ax.set_yticks(yticks)
@@ -223,12 +238,19 @@ def linePlot(
                 for i, _ in enumerate(yticks):
                     yticks[i] = round(yTickLabels[i * step], yDecimals)
                 ax.set_yticklabels(yticks)
-                ax.yaxis.set_major_formatter(FormatStrFormatter(f'%.{yDecimals}f'))
+                ax.yaxis.set_major_formatter(FormatStrFormatter(f"%.{yDecimals}f"))
     elif yTicks != 0:
         ax.set_yticks(ticks=yticks, labels=yticks)
-        ax.yaxis.set_major_formatter(FormatStrFormatter(f'%.{yDecimals}f'))
+        ax.yaxis.set_major_formatter(FormatStrFormatter(f"%.{yDecimals}f"))
     else:
-        ax.tick_params(axis='y', which='both', left=False, labelleft=False, right=False, labelright=False)
+        ax.tick_params(
+            axis="y",
+            which="both",
+            left=False,
+            labelleft=False,
+            right=False,
+            labelright=False,
+        )
 
     if title != "":
         ax.set_title(title)
@@ -239,7 +261,7 @@ def linePlot(
 
     fig.axes.append(ax)
 
-    if len(time) == len(x) and len(time)>0:
+    if len(time) == len(x) and len(time) > 0:
         ax2 = ax.twiny()
         ax2.set_xlim(ax.get_xlim())
         if xTicks != 0:
@@ -247,20 +269,34 @@ def linePlot(
 
         if datetime.fromtimestamp(time[-1]).day == datetime.fromtimestamp(time[0]).day:
             if x[-1] > x[0]:
-                labels = [datetime.fromtimestamp(t).strftime("%H:%M") for t in np.interp(ax.get_xticks(), x, time)]
+                labels = [
+                    datetime.fromtimestamp(t).strftime("%H:%M")
+                    for t in np.interp(ax.get_xticks(), x, time)
+                ]
             else:
-                labels = [datetime.fromtimestamp(t).strftime("%H:%M") for t in np.interp(ax.get_xticks(), np.flip(x), np.flip(time))]
+                labels = [
+                    datetime.fromtimestamp(t).strftime("%H:%M")
+                    for t in np.interp(ax.get_xticks(), np.flip(x), np.flip(time))
+                ]
             ax2.set_xticklabels(labels)
             ax2.set_xlabel("Time (HH:MM)")
         else:
             if x[-1] > x[0]:
-                labels = [datetime.fromtimestamp(t).strftime("%H:%M (%d/%m)") for t in np.interp(ax.get_xticks(), x, time)]
+                labels = [
+                    datetime.fromtimestamp(t).strftime("%H:%M (%d/%m)")
+                    for t in np.interp(ax.get_xticks(), x, time)
+                ]
             else:
-                labels = [datetime.fromtimestamp(t).strftime("%H:%M (%d/%m)") for t in np.interp(ax.get_xticks(), np.flip(x), np.flip(time))]
+                labels = [
+                    datetime.fromtimestamp(t).strftime("%H:%M (%d/%m)")
+                    for t in np.interp(ax.get_xticks(), np.flip(x), np.flip(time))
+                ]
             ax2.set_xticklabels(labels)
             ax2.set_xlabel("Time (HH:MM (dd/mm))")
 
-        ax2.tick_params('x', rotation=30)
+        ax2.tick_params("x", rotation=30)
         fig.axes.append(ax2)
 
-    fig.savefig(fName, bbox_inches="tight", transparent=kwargs.get("transparent", False))
+    fig.savefig(
+        fName, bbox_inches="tight", transparent=kwargs.get("transparent", False)
+    )
